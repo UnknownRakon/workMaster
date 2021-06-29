fb07fed29ce89b9a99fa9814697f94f1176839e4
Vlad <gorenfeo@gmail.com>
2021-06-29 20:50:57 +0300

    addReviews

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import BlogCategory, BlogPost, BlogType, Comments, User
+from .models import BlogCategory, BlogPost, BlogType, Comments, User, Reviews
 
 admin.site.register(BlogPost)
 admin.site.register(BlogCategory)
 admin.site.register(BlogType)
 admin.site.register(User)
 admin.site.register(Comments)
+admin.site.register(Reviews)
